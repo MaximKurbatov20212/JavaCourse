4590b0a53a931131b5a7b968d3d0ae00c9028274
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2022-06-11 20:12:24 +0700

    removed redundant field

--- BencodeToJsonParser/src/main/java/com/github/maxwell/bencodeparser/reporter/ErrorReporter.java
+++ BencodeToJsonParser/src/main/java/com/github/maxwell/bencodeparser/reporter/ErrorReporter.java
@@ -1,21 +1,19 @@
 package com.github.maxwell.bencodeparser.reporter;
 
 public class ErrorReporter {
-    private final int MAX_MESSAGES = 20;
+    private static final int MAX_MESSAGES = 20;
     private int nMessages;
-    private boolean canPrint = true;
 
     public void report(String message) {
         nMessages++;
         System.err.println(message);
         if (nMessages >= MAX_MESSAGES) {
             System.err.println("Too many errors, stopped...");
-            canPrint = false;
         }
     }
 
     public boolean tooManyErrors() {
-        return !canPrint;
+        return nMessages >= MAX_MESSAGES;
     }
     public boolean hasErrors() {
         return nMessages > 0;
